7d452c14c8135bfdbe906fb1c319e3e932d0361f
Reece Sellars <rsellars355@gmail.com>
2015-09-29 14:45:58 +0100

    Removed some unnecessary commented code and retabbed middleware file

--- src/Middleware/BeforeMiddleware.php
+++ src/Middleware/BeforeMiddleware.php
@@ -7,42 +7,25 @@ use Illuminate\Contracts\Foundation\Application;
 use Closure;
 
 class BeforeMiddleware implements Middleware {
-    
+
     protected $app;
 
     public function __construct(Application $app)
     {
         $this->app = $app;
     }
-    
+
     /**
-     * Tracker param
-     * @var string
-     */
-    // protected $testerserviceprovider;
-    
-    /**
-     * Constructor.
+     * Handle an incoming request.
      *
-     * @param SessionInterface $session
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
      */
-    /* public function __construct(TesterServiceProvider $testerserviceprovider)
+    public function handle($request, Closure $next)
     {
-        $this->testerserviceprovider = $testerserviceprovider;
+        $this->app['ab']->track($request);
+        return $next($request);
     }
-    */
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-       //$tester = new TesterServiceProvider();
-       $this->app['ab']->track($request);
-		return $next($request);
-	}
 
 }
